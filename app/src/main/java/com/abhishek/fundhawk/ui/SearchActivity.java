de172cc90207f9ec45f36b86fd3596e60137c0c0
Abhishek Tiwari <abhitiwari472@gmail.com>
2018-08-20 23:32:41 +0530

    Added databinding support dependency in gradle and updated activity_search.xml to support databinding

--- app/src/main/java/com/abhishek/fundhawk/ui/SearchActivity.java
+++ app/src/main/java/com/abhishek/fundhawk/ui/SearchActivity.java
@@ -1,11 +1,14 @@
 package com.abhishek.fundhawk.ui;
 
 import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.abhishek.fundhawk.R;
+import com.abhishek.fundhawk.databinding.ActivitySearchBinding;
 
 import javax.inject.Inject;
 
@@ -19,10 +22,16 @@ public class SearchActivity extends DaggerAppCompatActivity {
 
     private SearchActivityViewModel viewModel;
 
+    private ActivitySearchBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         AndroidInjection.inject(this);
+        viewModel = ViewModelProviders.of(this).get(SearchActivityViewModel.class);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
+        binding.setViewModel(viewModel);
+
     }
 }
